4cac540a17c8fb5a9015b2f9fa4e07aa9b0cb6f4
xieyipeng <3239202719@qq.com>
2020-06-21 11:07:41 +0800

    util3 finish

--- lihang/code/3-2.py
+++ lihang/code/3-2.py
@@ -5,54 +5,39 @@ n = T.shape[0]
 k = T.shape[1]
 
 
-# border = np.zeros((n, 2))
-
-
 class Node(object):
+    """
+    节点类，用于创建二叉树
+    """
     def __init__(self, item=None):
         self.elem = item
         self.l_child = None
         self.r_child = None
 
 
-class Tree(object):
-    def __init__(self, root=Node()):
-        self.root = root
-
-    def add(self, item):
-        node = Node(item)
-        if self.root is None:
-            self.root = node
-            return
-        queue = [self.root]
-        while queue:
-            cur_node = queue.pop(0)
-            if cur_node.l_child is None:
-                cur_node.l_child = node
-                return
-            else:
-                queue.append(cur_node.l_child)
-            if cur_node.r_child is None:
-                cur_node.r_child = node
-                return
-            else:
-                queue.append(cur_node.r_child)
-
-    def __str__(self):
-        return self.root.elem
-
-
-def dfs(node):
-    print('dfs')
+def traversal(node, mes):
+    """
+    二叉树遍历
+    :param node: 根节点
+    :param mes: 节点信息
+    :return: None
+    """
     if node is None:
         print('none')
         return
-    print(node.elem, end=' ')
-    dfs(node.l_child)
-    dfs(node.r_child)
+    print(node.elem, mes)
+    traversal(node.l_child, 'l')
+    traversal(node.r_child, 'r')
 
 
 def build_kd_tree(_node, _depth, m_set):
+    """
+    创建二叉树
+    :param _node: 待插入node节点
+    :param _depth: 该node深度
+    :param m_set: 待选定数据坐标集合
+    :return: None
+    """
     global k
     if len(m_set) == 0:
         return
@@ -63,20 +48,34 @@ def build_kd_tree(_node, _depth, m_set):
     # print(m_sort_set)
     point = m_sort_set[mid]
     # print('choose: ', point)
-    _node = Node(point)
-    print(_node.elem)
+    _node.elem = point
+    # print(_node.elem)
     l_set = []
     r_set = []
     for _i in range(0, mid):
         l_set.append(m_sort_set[_i])
     for _i in range(mid + 1, len(m_set)):
         r_set.append(m_sort_set[_i])
-    build_kd_tree(_node.l_child, _depth + 1, l_set)
-    build_kd_tree(_node.r_child, _depth + 1, r_set)
+
+    if len(l_set) is not 0:
+        _node.l_child = Node()
+        build_kd_tree(_node.l_child, _depth + 1, l_set)
+    if len(r_set) is not 0:
+        _node.r_child = Node()
+        build_kd_tree(_node.r_child, _depth + 1, r_set)
+
+
+def main():
+    """
+    主函数
+    :return: None
+    """
+    kd_node = Node()
+    build_kd_tree(kd_node, 0, T)
+    traversal(kd_node, 'root')
 
 
 if __name__ == '__main__':
-    kd_tree = Tree()
-    print(type(kd_tree.root))
-    build_kd_tree(kd_tree.root, 0, T)
-    dfs(kd_tree.root)
+    main()
+
+
--- lihang/util3.md
+++ lihang/util3.md
@@ -79,4 +79,116 @@ $$
   * 直到两个子区域没有实例存在时停止，形成$kd$树的区域划分。
 
 * 例3.2
+```python
+import numpy as np
 
+T = np.array([[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]])
+n = T.shape[0]
+k = T.shape[1]
+
+
+class Node(object):
+    """
+    节点类，用于创建二叉树
+    """
+    def __init__(self, item=None):
+        self.elem = item
+        self.l_child = None
+        self.r_child = None
+
+
+def traversal(node, mes):
+    """
+    二叉树遍历
+    :param node: 根节点
+    :param mes: 节点信息
+    :return: None
+    """
+    if node is None:
+        print('none')
+        return
+    print(node.elem, mes)
+    traversal(node.l_child, 'l')
+    traversal(node.r_child, 'r')
+
+
+def build_kd_tree(_node, _depth, m_set):
+    """
+    创建二叉树
+    :param _node: 待插入node节点
+    :param _depth: 该node深度
+    :param m_set: 待选定数据坐标集合
+    :return: None
+    """
+    global k
+    if len(m_set) == 0:
+        return
+    cut_dim = _depth % k
+    len_set = len(m_set)
+    mid = len_set // 2
+    m_sort_set = sorted(m_set, key=lambda x: x[cut_dim])
+    # print(m_sort_set)
+    point = m_sort_set[mid]
+    # print('choose: ', point)
+    _node.elem = point
+    # print(_node.elem)
+    l_set = []
+    r_set = []
+    for _i in range(0, mid):
+        l_set.append(m_sort_set[_i])
+    for _i in range(mid + 1, len(m_set)):
+        r_set.append(m_sort_set[_i])
+
+    if len(l_set) is not 0:
+        _node.l_child = Node()
+        build_kd_tree(_node.l_child, _depth + 1, l_set)
+    if len(r_set) is not 0:
+        _node.r_child = Node()
+        build_kd_tree(_node.r_child, _depth + 1, r_set)
+
+
+def main():
+    """
+    主函数
+    :return: None
+    """
+    kd_node = Node()
+    build_kd_tree(kd_node, 0, T)
+    traversal(kd_node, 'root')
+
+
+if __name__ == '__main__':
+    main()
+```
+* 输出结果
+
+```python
+[7 2] root
+[5 4] l
+[2 3] l
+none
+none
+[4 7] r
+none
+none
+[9 6] r
+[8 1] l
+none
+none
+none
+```
+### **3.3.2 搜索$kd$树**
+* 通过kd树搜索最近邻
+* 算法3.3
+
+<div align=center>
+<img src="./pic/QQ截图20200621110635.png"/>
+</div>
+
+<div align=center>
+<img src="./pic/QQ截图20200621110532.png"/>
+</div>
+> 以上图片来自：
+————————————————
+版权声明：本文为CSDN博主「cnblogs.com/qizhou/」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/qq_37189298/article/details/104164718
