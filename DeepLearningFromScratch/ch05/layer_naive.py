147d41763ea4de733680841dd7337c34805daf3a
xieyipeng <3239202719@qq.com>
2021-02-09 19:37:12 +0800

    scratch-P154

---
+++ DeepLearningFromScratch/ch05/layer_naive.py
@@ -0,0 +1,75 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx, dy
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
+if __name__ == '__main__':
+    apple = 100
+    apple_num = 2
+    tax = 1.1
+
+    # layer
+    mul_apple_layer = MulLayer()
+    mul_tax_layer = MulLayer()
+
+    # forward
+    apple_price = mul_apple_layer.forward(apple, apple_num)
+    price = mul_tax_layer.forward(apple_price, tax)
+
+    print("forward: " + str(price))
+
+    # backward
+    dprice = 1
+    dapple_price, dtax = mul_tax_layer.backward(dprice)
+    dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+    print("backward: " + str(dapple), str(dapple_num), str(dtax))
+    print("----------")
+    orange = 150
+    orange_num = 3
+
+    mul_orange_layer = MulLayer()
+    add_apple_orange_layer = AddLayer()
+
+    # forward
+    orange_price = mul_orange_layer.forward(orange, orange_num)
+    all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+    price = mul_tax_layer.forward(all_price, tax)
+
+    # backward
+    dprice = 1
+    dall_price, dtax = mul_tax_layer.backward(dprice)
+    dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+    dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+    dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+    print(price)
+    print(dapple_num, dapple, dorange, dorange_num, dtax)
+   
